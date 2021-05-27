7e73bdbe6d3445a2854a1162a3cdfd3a20795f43
Caique Noboa <caique.noboa@mercadolivre.com>
2021-05-27 11:04:53 -0300

    Data valid

--- src/main/java/com/mercadolibre/calculadora_idade/CalculadoraRestController.java
+++ src/main/java/com/mercadolibre/calculadora_idade/CalculadoraRestController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -14,14 +18,28 @@ import java.util.GregorianCalendar;
 public class CalculadoraRestController {
 
     @GetMapping("/{day}/{month}/{year}")
-    public String calcule(@PathVariable String day, @PathVariable String month, @PathVariable String year) throws ParseException {
+    public String calcule(@PathVariable String day, @PathVariable String month, @PathVariable String year) {
 
         Calendar dateOfBirth = new GregorianCalendar();
-        dateOfBirth.setTime(new SimpleDateFormat("dd/MM/yyyy").parse(day + "/" + month + "/" + year));
+        Date date = null;
+        try{
+            date = Date.from(LocalDate.parse(day + "/" + month + "/" + year, DateTimeFormatter.ofPattern("dd/MM/uuuu").withResolverStyle(ResolverStyle.STRICT)).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        }
+        catch (Exception e){
+            return "Data Invalida";
+        }
+
+        dateOfBirth.setTime(date);
+
+
+
+        System.out.println(dateOfBirth.toString());
 
         Calendar today = Calendar.getInstance();
 
         int age = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
+
 
         dateOfBirth.add(Calendar.YEAR, age);
 
